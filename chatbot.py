96cd91e4312d70146b1cd362de581e68084a3677
achetanvarma <chetanvarmaatla@gmail.com>
2025-10-08 19:55:25 +0530

    Adding files of 08102025

--- chatbot.py
+++ chatbot.py
@@ -59,3 +59,57 @@ def preprocess_data(gender, married, dependents, education, employed, credit, ar
     except Exception as e:
         st.error(f"Error in preprocessing: {str(e)}")
         return None
+    
+def show_chatbot():
+    st.title("🤖 Loan Prediction Chatbot")
+    st.markdown("""Chat with our AI - Powered chatbot to get perosonalized loan predictios. Simply answer the question, and the chatbot will guide you""")
+    
+    #Initialize session state
+
+    intialize_session_start()
+
+    # Define questions 
+
+    questions =["What is your gender? (Male/Female)",
+                "Are you married? (Yes/No)",
+                "How many dependents do you have? (0/1/2/3+)",
+                "What is your education level? (Graduate/Not Graduate)",
+                "Are you self-employed? (Yes/No)",
+                "What is your monthly applicant income?",
+                "What is your monthly co-applicant income?",
+                "What is the loan amount you are requesting?",
+                "What  is the loan term in months?",
+                "What is you credit history score?(300-850)",
+                "What is the area?(Urban/Semiurban/Rural)"
+                ]
+    #Display chat history
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    #{"role":user,content:"hello"}
+
+
+    #show initial greeting if not started
+    if not st.session_state.started and st.session_state.current_step == -1:
+        with st.chat_message("assistant"):
+            st.write("Hello! I'm you loan eligibility assistant. Are you ready to begin? (Yes/No)")
+
+    # get User input
+
+    user_input =st.chat_input("Your answer:")
+
+    if user_input:
+
+        if not st.session_state.started:
+            st.session_state.messages.append({"role":"user","content":user_input})
+
+            if user_input.lower()=="yes":
+                st.session_state.started =True
+                st.session_state.current_step = 0
+                st.session_state.messages.append({
+                    "role":"assisant",
+                    "content":'''Great! Let's get started with your loan eligibility assessment:
+                    what is your gender? (Male/Female)'''
+                })
+                st.session_state.show_next_question=True
